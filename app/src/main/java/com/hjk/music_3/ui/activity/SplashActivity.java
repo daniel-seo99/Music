089d4a5784f141e08d9bf8622506601a909501ab
hojong94 <70558345+hojong94@users.noreply.github.com>
2020-09-01 13:25:25 +0900

    k

--- app/src/main/java/com/hjk/music_3/ui/activity/SplashActivity.java
+++ app/src/main/java/com/hjk/music_3/ui/activity/SplashActivity.java
@@ -59,7 +59,7 @@ public class SplashActivity extends AppCompatActivity {
         });
 
         String Email="hjk";
-  
+        String Pwd="123";
 
         userViewModel.getLogin(Email).observe(this, userViewModel->{
 
